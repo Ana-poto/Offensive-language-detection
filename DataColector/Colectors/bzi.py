8d655848b217e37ff615a244971684d360bc048e
Ana Potoroaca <53191102+Ana-poto@users.noreply.github.com>
2021-04-16 23:35:12 +0300

    Files for annotations

--- DataColector/Colectors/bzi.py
+++ DataColector/Colectors/bzi.py
@@ -10,11 +10,11 @@ def getComments(URL):
     comments = []
 
     for coment in content:
-        comments.append("ANNOTATION\n")
         comments.append(coment.text)
 
     f = open("E:/GitHub/Offensive-language-detection/DataColector/ColectingPhase2/bzi.txt", "a", encoding="utf-8")
     for com in comments:
+        f.write("ANNOTATION\n")
         f.write(str(com))
     f.close()
 
